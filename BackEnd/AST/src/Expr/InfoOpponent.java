9eb2b4e9abdaa5e9c76bf03958f0b87101c9ff73
Suppakorn <suwannapop01@gmail.com>
2024-02-14 21:30:36 +0700

    Improve InfoOpponent.java

--- BackEnd/AST/src/Expr/InfoOpponent.java
+++ BackEnd/AST/src/Expr/InfoOpponent.java
@@ -10,17 +10,25 @@ public class InfoOpponent implements  Expression{
         String[] Direction = {"up","upright","downright" , "down" , "downleft","upleft"};
         Region current = user.cityCrew;
         int minDistance = Integer.MAX_VALUE ;
+        int MaxofMap = user.territory().getTerritory_col() * user.territory().getTerritory_row();
         for (int i = 0; i < 6; i++) {
-            for (int distance = 1; distance < minDistance ; distance++) {
+            for (int distance = 1; distance <= MaxofMap ; distance++) {
                 current = current.moveDirection(Direction[i]);
                 if (current.owner != null && current.owner != user){
-                    int temp = 10*distance + i + 1;
-                    if(temp < minDistance) minDistance = temp ;
+                    int temp = 10 * distance + i + 1;
+                    if(temp < minDistance) {
+                        minDistance = temp ;
+                        MaxofMap = distance ;
+                    }
                 }
             }
             current = user.cityCrew;
         }
-        if (minDistance == Integer.MAX_VALUE){return 0 ;} else{ return minDistance;}
+        if (minDistance == Integer.MAX_VALUE){
+            return 0 ;
+        } else{
+            return minDistance;
+        }
     }
 
     @Override
