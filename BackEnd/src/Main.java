e6dd4905b070e751260ff696415cb25dc3a0abb7
Suppakorn <110958686+Suwannapop@users.noreply.github.com>
2024-02-20 09:20:42 +0700

    Fix bug in ConstructionPlanParser.class

--- BackEnd/src/Main.java
+++ BackEnd/src/Main.java
@@ -1,15 +1,15 @@
+import Expr.TokenizerOneLine;
+
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) throws Exception {
         int i = 0;
         FileManager reader = new FileManager();
-        List<String> file =  reader.FileReader("BackEnd/src/constructionplanWithcomment.txt");
-        Tokenizer test = new Tokenizer(file);
-        ConstructionPlanParser x = new ConstructionPlanParser(test);
-        x.parse();
-
+        List<String> file =  reader.FileReader("BackEnd/src/constructionplanWithcomment1.txt");
+        String y = file.toString();
+        TokenizerOneLine tokenizer = new TokenizerOneLine(y);
+        System.out.println(y);
     }
 }
