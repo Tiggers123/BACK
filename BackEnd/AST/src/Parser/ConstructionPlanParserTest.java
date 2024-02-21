4b65b7ee1e48c25975acd1fc4b18cd779da241a0
Suppakorn <110958686+Suwannapop@users.noreply.github.com>
2024-02-21 18:04:18 +0700

    Test move complate ConstructionPlanParserTest.class

--- BackEnd/AST/src/Parser/ConstructionPlanParserTest.java
+++ BackEnd/AST/src/Parser/ConstructionPlanParserTest.java
@@ -1,30 +1,25 @@
 package Parser;
 
-import Expr.ConstructionPlan;
 import GameState.Player;
 import GameState.Territory;
 
 import java.util.ArrayList;
 import java.util.List;
-import Statement.Statement;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConstructionPlanParserTest {
     private List<String> Command(String command){
-        FileManager reader = new FileManager();
         List<String> file =  new ArrayList<>();
         file.add(command);
         return file;
     }
-    public Player setUpGame() throws SyntaxErrorException {
+    public Player setUpGame()  {
         String[] p = {"Ton"};
         Territory map = new Territory(5, 5, 1100, 1000, 100, 100, 100, p);
         List<Player> p1 = map.getPlayer();
-        Player p2= p1.get(0);
-        return p2;
+        return p1.get(0);
     }
     @Test
     void TopLeftConnerTest() throws SyntaxErrorException, Expr.SyntaxErrorException {
@@ -33,7 +28,7 @@ class ConstructionPlanParserTest {
         int row = territory.getTerritory_row();
         int col = territory.getTerritory_col();
         int longest = Math.max(row , col);
-        int shortest = Math.min(row , col);
+//        int shortest = Math.min(row , col);
         double longestpow = Math.pow(longest,2);
         double c = Math.sqrt(longestpow+longestpow);
         c = Math.ceil(c);
@@ -93,7 +88,6 @@ class ConstructionPlanParserTest {
         Player.blink(territory , 0 , 0);
         p = Command("move downleft");
         Player.setPlan(p);
-        i = 0 ;
         for (int j = 0; j < c ; j++) {
             assertNotEquals(null,Player.getCityCrew());
             Player.evaluatePlan();
@@ -103,7 +97,6 @@ class ConstructionPlanParserTest {
         Player.blink(territory , 0 , 0);
         p = Command("move upleft");
         Player.setPlan(p);
-        i = 0 ;
         for (int j = 0; j < c ; j++) {
             assertNotEquals(null,Player.getCityCrew());
             assertEquals(territory.getRegion(0,0),Player.getCityCrew());
@@ -118,7 +111,7 @@ class ConstructionPlanParserTest {
         int row = territory.getTerritory_row();
         int col = territory.getTerritory_col();
         int longest = Math.max(row , col);
-        int shortest = Math.min(row , col);
+//        int shortest = Math.min(row , col);
         double longestpow = Math.pow(longest,2);
         double c = Math.sqrt(longestpow+longestpow);
         c = Math.ceil(c);
@@ -188,15 +181,12 @@ class ConstructionPlanParserTest {
         p = Command("move upleft");
         Player.setPlan(p);
         assertEquals(territory.getRegion(0,col-1),Player.getCityCrew());
-        i = 0 ;
         for (int j =col-1 ; j > 0 ; j--) {
+            Player.evaluatePlan();
+            assertNotEquals(null,Player.getCityCrew());
             if ((col-1) %2 == 0){
-                    Player.evaluatePlan();
-                    assertNotEquals(null,Player.getCityCrew());
-                    assertEquals(territory.getRegion(0,col-2),Player.getCityCrew());
+                assertEquals(territory.getRegion(0,col-2),Player.getCityCrew());
             }else {
-                Player.evaluatePlan();
-                assertNotEquals(null,Player.getCityCrew());
                 assertEquals(territory.getRegion(0,col-1),Player.getCityCrew());
             }
         }
@@ -208,7 +198,7 @@ class ConstructionPlanParserTest {
         int row = territory.getTerritory_row();
         int col = territory.getTerritory_col();
         int longest = Math.max(row , col);
-        int shortest = Math.min(row , col);
+//        int shortest = Math.min(row , col);
         double longestpow = Math.pow(longest,2);
         double c = Math.sqrt(longestpow+longestpow);
         c = Math.ceil(c);
@@ -281,12 +271,9 @@ class ConstructionPlanParserTest {
         int temp = 0;
         assertEquals(territory.getRegion(row-1,col-1),Player.getCityCrew());
         for (int j =col-1 ; j > 0  ; j--) {
-            if (j%2 == 0){
-                assertNotEquals(null,Player.getCityCrew());
-                assertEquals(territory.getRegion(i,col-1-temp),Player.getCityCrew());
-            }else {
-                assertNotEquals(null,Player.getCityCrew());
-                assertEquals(territory.getRegion(i,col-1-temp),Player.getCityCrew());
+            assertNotEquals(null,Player.getCityCrew());
+            assertEquals(territory.getRegion(i,col-1-temp),Player.getCityCrew());
+            if (j % 2 != 0) {
                 i--;
             }
             Player.evaluatePlan();
@@ -301,7 +288,7 @@ class ConstructionPlanParserTest {
         int row = territory.getTerritory_row();
         int col = territory.getTerritory_col();
         int longest = Math.max(row , col);
-        int shortest = Math.min(row , col);
+//        int shortest = Math.min(row , col);
         double longestpow = Math.pow(longest,2);
         double c = Math.sqrt(longestpow+longestpow);
         c = Math.ceil(c);
@@ -323,12 +310,9 @@ class ConstructionPlanParserTest {
         assertEquals(territory.getRegion(row-1,0),Player.getCityCrew());
         int i=row-1;
         for (int j =0 ; j < col-1  ; j++) {
-            if (j%2 == 0){
-                assertNotEquals(null,Player.getCityCrew());
-                assertEquals(territory.getRegion(i, temp),Player.getCityCrew());
-            }else {
-                assertNotEquals(null,Player.getCityCrew());
-                assertEquals(territory.getRegion(i, temp),Player.getCityCrew());
+            assertNotEquals(null,Player.getCityCrew());
+            assertEquals(territory.getRegion(i, temp),Player.getCityCrew());
+            if (j % 2 != 0) {
                 i--;
             }
             Player.evaluatePlan();
@@ -340,8 +324,6 @@ class ConstructionPlanParserTest {
         Player.blink(territory , row-1 , 0);
         p = Command("move downright");
         Player.setPlan(p);
-        i = row-1 ;
-        temp = 0;
         assertEquals(territory.getRegion(row-1,0),Player.getCityCrew());
         for (int j =col-1 ; j > 0  ; j--) {
             assertNotEquals(null,Player.getCityCrew());
@@ -351,7 +333,6 @@ class ConstructionPlanParserTest {
 
         // Move Down
         Player.blink(territory , row-1, 0);
-        i = row-1 ;
         p = Command("move down");
         Player.setPlan(p);
         for (int j = 0; j < row ; j++){
@@ -364,7 +345,6 @@ class ConstructionPlanParserTest {
         Player.blink(territory , row-1 , 0);
         p = Command("move downleft");
         Player.setPlan(p);
-        i = row-1 ;
         for (int j = col-1; j >= 0 ; j--){
             assertNotEquals(null,Player.getCityCrew());
             assertEquals(territory.getRegion(row-1,0),Player.getCityCrew());
@@ -375,7 +355,6 @@ class ConstructionPlanParserTest {
         Player.blink(territory , row-1 , 0);
         p = Command("move upleft");
         Player.setPlan(p);
-        i = 0 ;
         for (int j = 0; j < c ; j++) {
             assertNotEquals(null,Player.getCityCrew());
             assertEquals(territory.getRegion(row-1 , 0),Player.getCityCrew());
@@ -389,11 +368,11 @@ class ConstructionPlanParserTest {
         int row = territory.getTerritory_row();
         int col = territory.getTerritory_col();
         int longest = Math.max(row , col);
-        int shortest = Math.min(row , col);
+//        int shortest = Math.min(row , col);
         double longestpow = Math.pow(longest,2);
         double c = Math.sqrt(longestpow+longestpow);
         c = Math.ceil(c);
-        int i = 0 ;
+        int i;
         int midRow = (row-1)/2;
         int midCol = (col-1)/2;
 
@@ -449,7 +428,6 @@ class ConstructionPlanParserTest {
 
         // Move Down
         Player.blink(territory , midRow , midCol);
-        i = row-1 ;
         p = Command("move down");
         Player.setPlan(p);
         for (int j = midRow; j <= row; j++) {
