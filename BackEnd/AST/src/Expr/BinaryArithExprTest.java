248fe03903b790777a9ad23c71926744488d840e
Okinawa <fitthep@gmail.com>
2024-02-27 21:49:58 +0700

    BinaryArithExprTest
    unfinished
    -@Test testConstructionPlan()
    Command("b = budget - 25")

--- BackEnd/AST/src/Expr/BinaryArithExprTest.java
+++ BackEnd/AST/src/Expr/BinaryArithExprTest.java
@@ -159,7 +159,6 @@ import static org.junit.jupiter.api.Assertions.*;
         player.setPlan(p);
         player.evaluatePlan();
         assertEquals(budget - 25, player.variable.get("b"));
-
     }
 
 
