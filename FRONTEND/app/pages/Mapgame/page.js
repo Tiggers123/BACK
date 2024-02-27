836d17008ede2e9dc24aec0e8705aa23210bee78
tigerlab <tiggers12@hotmail.com>
2024-02-27 16:10:06 +0700

    fix gamepage

--- FRONTEND/app/pages/Mapgame/page.js
+++ FRONTEND/app/pages/Mapgame/page.js
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import styles from "./mapgame.module.css";
 import HexGrid from "../components/HexGrid";
@@ -6,9 +5,9 @@ import HexGrid from "../components/HexGrid";
 const Mapgame = () => {
   return (
     <div className={styles.bg}>
-      <div class="rpgui-content">
+      <div className="rpgui-content">
         <div
-          class="rpgui-container framed"
+          className="rpgui-container framed"
           style={{
             width: "900px",
             height: "650px",
@@ -20,6 +19,68 @@ const Mapgame = () => {
           }}
         >
           <HexGrid row={15} column={15} />
+        </div>
+        <div
+          className="rpgui-container framed-golden-2"
+          style={{
+            width: "500px",
+            height: "675px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "30px",
+            marginLeft: "1000px",
+          }}
+        >
+          <div
+            className="rpgui-container" // Keep OPPONENT TURN and <hr> outside of the white box
+            style={{
+              marginBottom: "500px",
+            }}
+          >
+            <span
+              style={{
+                fontFamily: "hello",
+                fontSize: "30px",
+                textAlign: "center",
+              }}
+            >
+              OPPONENT TURN
+            </span>
+            <hr className="golden" style={{ marginTop: "20px" }}></hr>
+          </div>
+          <div className={styles.box}>
+            <div
+              class="rpgui-icon sword"
+              style={{ marginRight: "-10px", marginBottom: "-15px" }}
+            ></div>{" "}
+            {/* Add margin-right */}
+            DEPOSIT:100000
+            <br />
+            <br />
+            <div
+              class="rpgui-icon shield"
+              style={{ marginRight: "-10px", marginBottom: "-15px" }} // Add negative margin-top
+            ></div>{" "}
+            {/* Add margin-right */}
+            BUDGET:100000
+            <br />
+            <br />
+            <div
+              class="rpgui-icon potion-red"
+              style={{ marginRight: "-10px", marginBottom: "-15px" }} // Add negative margin-top
+            ></div>{" "}
+            {/* Add margin-right */}
+            COLUMN:20
+            <br />
+            <br />
+            <div
+              class="rpgui-icon potion-blue"
+              style={{ marginRight: "-10px", marginBottom: "-15px" }} // Add negative margin-top
+            ></div>{" "}
+            {/* Add margin-right */}
+            ROW:20
+          </div>
         </div>
       </div>
     </div>
