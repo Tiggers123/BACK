b434e3c2785ea4e0f6460c51875da58482493c2f
tigerlab <tiggers12@hotmail.com>
2024-02-29 01:35:37 +0700

    fix inputname

--- FRONTEND/app/page.js
+++ FRONTEND/app/page.js
@@ -1,8 +1,23 @@
-import Link from "next/link";
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./home.module.css";
 
 const Home = () => {
+  const [name, setName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleJoinGame = () => {
+    if (name.trim() === "") {
+      setErrorMessage("Please input your name.");
+    } else {
+      window.location.href = "/pages/menu";
+    }
+  };
+
   return (
     <div className="outer-container">
       <div className="inner-container"></div>
@@ -12,12 +27,27 @@ const Home = () => {
           type="text"
           placeholder="Please input your name..."
           className={styles.nameinput}
+          value={name}
+          onChange={handleInputChange}
         />
-        <button className="button">
-          <Link href="/pages/menu">
-            <span className="button-text">Join Game</span>
-            <div className="fill-container"></div>
-          </Link>
+        {errorMessage && (
+          <p
+            style={{
+              color: "red",
+              fontFamily: "Loveded",
+              textAlign: "center",
+              fontSize: "25px",
+              marginLeft: "72px",
+              marginTop: "20px",
+              marginBottom: "-15px",
+            }}
+          >
+            {errorMessage}
+          </p>
+        )}
+        <button className="button" onClick={handleJoinGame}>
+          <span className="button-text">Join Game</span>
+          <div className="fill-container"></div>
         </button>
       </div>
     </div>
