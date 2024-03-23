3f738e5db59a29599a6a9c43bb5d420a5d6fdcc7
Suppakorn <suwannapop01@gmail.com>
2024-03-23 17:43:12 +0700

    Refactor Code From Old BackEND to WebSocket

--- UpbeatWebSocket/src/main/java/com/example/UpbeatWebSocket/UpbeatWebSocketApplication.java
+++ UpbeatWebSocket/src/main/java/com/example/UpbeatWebSocket/UpbeatWebSocketApplication.java
@@ -3,11 +3,15 @@ package com.example.UpbeatWebSocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class UpbeatWebSocketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UpbeatWebSocketApplication.class, args);
-	}
 
+		SpringApplication.run(UpbeatWebSocketApplication.class, args);
+
+	}
 }
+
